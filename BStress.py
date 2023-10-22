d005fcc16fe5f4ca69aae14c76b218ca324f68b0
luchernandezse <luchernandezse@gmail.com>
2023-10-21 21:03:26 -0500

    Se completa grafica pero no se ha ajustado grafica para su visualizacion

--- BStress.py
+++ BStress.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+V=float(10)
+print(type(V))
 b1=0.2
 b2=0.1
 b3=0.2
@@ -11,61 +13,39 @@ A=[b1*h1,b2*h2,b3*h3]
 y=[h1/2,h1+(h2/2),h1+h2+(h3/2)]
 I=[b1*(h1**3)/12,b2*(h2**3)/12,b3*(h3**3)/12]
 if len(A) != len(y):
-    raise ValueError("Los vectores deben tener la misma longitud")
+    raise ValueError("Vector's need to have same length. Check!")
 Ay = []
 for i in range(len(A)):
     Ay.append(A[i] * y[i])
 Xc= sum(Ay)/sum(A)
-print(sum(Ay))
-print(sum(A))
-print(A)
-print(y)
-print(I)
-print("El centroide de la figura esta a "+str(Xc)+" m desde la cara inferior")
+#print("El centroide de la figura esta a "+str(Xc)+" m desde la cara inferior")
 d=[]
 for i in range(len(y)):
     d.append(A[i]*(abs(y[i]-Xc))**2)
 Ic=sum(I)+sum(d)
-print(Ic)
-
 ## plotear
 plt.style.use('_mpl-gallery')
 # make data
 x = np.linspace(0, h1+h2+h3, 100)
 print(x)
+q = np.zeros_like(x)
+T = np.zeros_like(x)
+print(type(T))
+q[(x<=Xc)&(x<=h1)] = x[(x<Xc)&(x<h1)]*b1*(Xc-x[(x<Xc)&(x<h1)]/2)
+q[(x<=Xc)&(x>h1)] = h1*b1*(Xc-h1/2)+b2*(x[(x<Xc)&(x>h1)]-h1)*(Xc-h1-(x[(x<Xc)&(x>h1)]-h1)/2)
+q[(x>Xc)&(x<=(h1+h2))] = h3*b3*(h-Xc-h3/2)+b2*(h1+h2-x[(x>Xc)&(x<=(h1+h2))])*(h1+h2-Xc-((h1+h2-x[(x>Xc)&(x<=(h1+h2))])/2))
+q[(x>Xc)&(x>(h1+h2))] = b3*(h-x[(x>Xc)&(x>(h1+h2))])*(h-Xc-((h-x[(x>Xc)&(x>(h1+h2))])/2))
+print(q)
+T[(x<=Xc)&(x<=h1)]=V*q[(x<=Xc)&(x<=h1)]/(Ic*b1)
+T[(x<=Xc)&(x>h1)]=V*q[(x<=Xc)&(x>h1)]/(Ic*b2)
+T[(x>Xc)&(x<=(h1+h2))]=V*q[(x>Xc)&(x<=(h1+h2))]/(Ic*b2)
+T[(x>Xc)&(x>(h1+h2))]=V*q[(x>Xc)&(x>(h1+h2))]/(Ic*b3)
 
-
-y = 4 + 2 * np.sin(2 * x)
-
-# plot
-fig, ax = plt.subplots()
-
-ax.plot(x, y, linewidth=2.0)
-
-ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ylim=(0, 8), yticks=np.arange(1, 8))
-
+plt.plot(T, x, label='Shear Stress')
+plt.axis((0,np.max(T)+10,0,h))
+plt.xlabel('Shear stress [KPa]')
+plt.ylabel('Beam height [m]')
+plt.title('Shear stress distribution on Beam section')
+plt.grid(True)
+plt.legend()
 plt.show()
-
-
-
-
-def gen_passw():
-    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
-    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
-    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
-    char=MAYUS+MINUS+NUMS+CHARS
-    password=[]
-    for i in range(15):
-        char_rand=random.choice(char)
-        password.append(char_rand)
-    password="".join(password)
-    return password
-
-def run(): 
-    password=gen_passw()
-    print('Your new password is: '+password)
-
-if __name__=='__main__':
-    run()
